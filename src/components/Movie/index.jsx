c4f3b43b80ea8b289d4b8bb328ca1c6acf0530fe
petra <halouzkovap@seznam.cz>
2021-10-12 17:52:17 +0200

    change css style

--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -8,21 +8,19 @@ import Actor from '../Actor'
 const Movie = (props)=>{
     return (
         <div className = "grid-container">
-        <div className=" posterItem">
+        <div className=" poster">
         <img src={props.poster} alt="poster" className="img" />
         </div>
-        <div className="grid-item descriptionItem">
+        <div className="description">
         <h3>{props.title}</h3>
         <p><b>Rok vydání</b>: {props.year}</p>
         <p><b>Žánr</b>: {props.genre}</p>
         <p><b>Režie</b>: {props.director}</p>
         <p><b>Rating</b>:{props.rating}/10</p>
+        <br/>
+        <h5>V hlavních rolích:</h5>
+         {props.actors.map(a=><Actor name={a.name} as={a.as}/>)}
         </div>
-        <div className="grid-item castItem">
-            <h4>V hlavních rolích:</h4>
-            {props.actors.map(a=><Actor name={a.name} as={a.as}/>)}
-            </div>
-        
         </div>
     )
 }
